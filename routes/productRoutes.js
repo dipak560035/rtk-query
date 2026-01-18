56e2fe7a9e7ebcc1df85c892aaa658a73452525f
dipak560035 <dipaksah2070@gmail.com>
2026-01-19 01:54:20 +0545

    apit

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,38 +1,4 @@
 
-// import express from 'express';
-
-
-// import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from '../controllers/productController.js';
-// import { notAllowed } from '../utils/notAllowed.js';
-// import { checkFile, updateCheckFile } from '../middlewares/checkFile.js';
-// import { checkId } from '../middlewares/checkId.js';
-// import { checkAdmin, checkUser } from '../middlewares/checkUser.js';
-
-
-
-// const router = express.Router();
-
-// router.route('/products')
-//   .get( getProducts)
-//   .post(checkUser,checkAdmin,checkFile, createProduct).all(notAllowed);
-
-// router.route('/products/:id')
-//   .get(checkId,getProduct)
-//   .patch(checkId,updateCheckFile,updateProduct)
-//   .delete(checkId,deleteProduct).all(notAllowed);
-
-//   export default router;
-
-
-
-
-
-
-
-
-
-
-
 
 import express from "express";
 import {
