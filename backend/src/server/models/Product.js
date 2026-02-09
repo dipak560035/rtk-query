4d5a5c256f5970646c7f73cebdca704f4bb3c978
dipak560035 <dipaksah2070@gmail.com>
2026-02-09 13:53:08 +0545

    review

--- backend/src/server/models/Product.js
+++ backend/src/server/models/Product.js
@@ -8,6 +8,16 @@ const imageSchema = new mongoose.Schema(
   { _id: false }
 )
 
+const reviewSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    name: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    comment: { type: String, required: true }
+  },
+  { timestamps: true, _id: false }
+)
+
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
@@ -19,6 +29,8 @@ const productSchema = new mongoose.Schema(
     tags: [{ type: String, index: true }],
     stock: { type: Number, default: 0 },
     rating: { type: Number, default: 0, min: 0, max: 5 },
+    numReviews: { type: Number, default: 0 },
+    reviews: [reviewSchema],
     colors: [{ type: String }],
     sizes: [{ type: String }],
     images: [imageSchema],
@@ -30,4 +42,3 @@ const productSchema = new mongoose.Schema(
 productSchema.index({ name: 'text', description: 'text', tags: 'text' })
 
 export const Product = mongoose.model('Product', productSchema)
-
