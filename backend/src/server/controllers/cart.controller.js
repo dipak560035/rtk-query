cb57185efae6895ca1615eedc44753e95f04475b
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-05-09 11:22:14 +0545

    Update cart.controller.js

--- backend/src/server/controllers/cart.controller.js
+++ backend/src/server/controllers/cart.controller.js
@@ -18,6 +18,100 @@ export const getCart = async (req, res) => {
   }
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { Cart } from '../models/cart.js';
+import { Product } from '../models/Product.js';
+
+// get cart
+export const getCart = async (req, res) => {
+  try {
+    let cart = await Cart.findOne({ user: req.user._id }).populate('items.product');
+
+    if (!cart) {
+      cart = await Cart.create({ user: req.user._id, items: [] });
+    }
+
+    res.json({ success: true, data: cart });
+  } catch (err) {
+    console.error("Get cart error:", err);
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
 // add to cart
 export const addToCart = async (req, res) => {
   try {
