2ea6f6b3a2be74030d417a85fc59764420d73027
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-05-09 11:23:11 +0545

    Update cart.controller.js

--- backend/src/server/controllers/cart.controller.js
+++ backend/src/server/controllers/cart.controller.js
@@ -1,6 +1,24 @@
 
 import { Cart } from '../models/cart.js';
+import { Product } frimport { Cart } from '../models/cart.js';
 import { Product } from '../models/Product.js';
+
+// get cart
+export const getCart = async (req, res) => {
+  try {
+    let cart = await Cart.findOne({ user: req.user._id }).populate('items.product');
+
+    if (!cart) {
+      cart = await Cart.create({ user: req.user._id, items: [] });
+    }
+
+    res.json({ success: true, data: cart });
+  } catch (err) {
+    console.error("Get cart error:", err);
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+om '../models/Product.js';
 import { Cimport { Cart } from '../models/cart.js';
 import { Product } from '../models/Product.js';
 
